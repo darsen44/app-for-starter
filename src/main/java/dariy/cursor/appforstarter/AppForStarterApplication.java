2d567ec8547ed7d22d99bdef3e1615c17b584125
darsen44 <dariy.senyshyn44@gmail.com>
2018-06-01 00:36:22 +0300

    initial commit

---
+++ src/main/java/dariy/cursor/appforstarter/AppForStarterApplication.java
@@ -0,0 +1,12 @@
+package dariy.cursor.appforstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppForStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppForStarterApplication.class, args);
+	}
+}
